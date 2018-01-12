4bc070df36289838bc575e7d7d01af0d43928aa8
Jason Cunning <jcunning@ucar.edu>
2018-01-11 17:14:40 -0700

    Add basic param validation to machine activity endpoint and exception handlers for report generation and validation

--- src/main/java/edu/ucar/cisl/controller/MachineActivityReportController.java
+++ src/main/java/edu/ucar/cisl/controller/MachineActivityReportController.java
@@ -4,24 +4,29 @@ import edu.ucar.cisl.config.Factory;
 import edu.ucar.cisl.report.machineactivity.MachineActivityReport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+@RestController
+@Validated
 public class MachineActivityReportController {
 
     @Autowired
     private Factory<MachineActivityReportQuery> queryFactory;
 
-    @Autowired @Qualifier(value ="machine")
+    @Autowired @Qualifier(value = "machine")
     private String machine;
 
-    @GetMapping("/v1/report/activity")
+    @GetMapping(value = "/v1/report/activity", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public MachineActivityReport getMachineActivityReport(@RequestParam Integer daysAgo) {
+    public MachineActivityReport getMachineActivityReport(
+            @Min(value = 10, message = "daysAgo must be between 10 and 100") @Max(value = 100, message = "daysAgo must be between 10 and 100") @RequestParam Integer daysAgo) {
         return queryFactory.create()
                 .machine(machine)
                 .daysAgo(daysAgo)
